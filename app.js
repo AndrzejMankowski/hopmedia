26b8a44c8682924cdf4a1fc830b270a38686c36f
Andrzej Mankowski <bialysmok.production@gmail.com>
2023-01-21 12:50:18 +0100

    Scroll and opacity vol. 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.querySelectorAll('nav a').forEach(element => {
 element.onclick = fuction (){
     const href = element.getAttribute('href');
+    document querySelectorAll(href);classList.add('shown');
 }
 })
